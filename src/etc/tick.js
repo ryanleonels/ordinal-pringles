c2e77e681a0373e309f0da28814421b6275e01c0
Ryan Leonel Somali <ryan.somali@team.bumble.com>
2024-04-02 09:59:38 +0100

    remove ordinal cap on BHO @ base 2

--- src/etc/tick.js
+++ src/etc/tick.js
@@ -72,7 +72,7 @@ function tick(diff){
     }
 
     // Markup Autobuyer
-    let collapseCheck = data.ord.ordinal.lt(BHO_VALUE) || data.collapse.times > 0
+    let collapseCheck = data.ord.ordinal.lt(BHO_VALUE) || data.collapse.times > 0 || data.ord.base != 3
     let boostCheck = data.boost.times > 0 || data.collapse.hasSluggish[0]
     if(timesToLoop[3].gte(1) && data.ord.isPsi && data.autoStatus.enabled[1] && !boostCheck && data.ord.isPsi) data.ord.ordinal = D(GRAHAMS_VALUE)
     if(timesToLoop[3].gte(1) && data.ord.isPsi && data.autoStatus.enabled[1] && collapseCheck && boostCheck) markup(timesToLoop[3].times(diff/1000))
